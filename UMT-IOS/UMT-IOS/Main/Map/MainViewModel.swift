42a583db201999921771c7828636cffa76337b4e
이진하 <dkwlsfk2323@gmail.com>
2022-03-05 20:07:29 +0900

    add makeMark function in MainViewModel

--- UMT-IOS/UMT-IOS/Main/Map/MainViewModel.swift
+++ UMT-IOS/UMT-IOS/Main/Map/MainViewModel.swift
@@ -7,15 +7,24 @@
 
 import UIKit
 import PanControllerHeight
+import GoogleMaps
 
 class MainViewModel {
-    let viewController: UIViewController
+    let viewController: MapViewController
     init(viewController: UIViewController) {
-        self.viewController = viewController
+        self.viewController = viewController as! MapViewController
     }
     func showSheetVC() {
         let sheetVC = SheetViewController()
         sheetVC.configurePanSetting(viewController: sheetVC, defaultHeight: 300, maxHeight: 600)
         viewController.presentPanViewController(viewController: sheetVC)
+    }
+    
+    func makeMark() {
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 33, height: 44))
+        imageView.image = UIImage(named: "mark_normal")
+        let gmsMarker = GMSMarker(position: CLLocationCoordinate2D(latitude: -33.90, longitude: 151.20))
+        gmsMarker.iconView = imageView
+        gmsMarker.map = viewController.mapView
     }
 }
--- UMT-IOS/UMT-IOS/Main/Map/MapViewController.swift
+++ UMT-IOS/UMT-IOS/Main/Map/MapViewController.swift
@@ -13,16 +13,20 @@ class MapViewController: UIViewController {
 
     //MARK: - Properties
     var viewModel: MainViewModel?
+    var mapView = GMSMapView()
     
     //MARK: - Life Cycles
     override func viewDidLoad() {
             super.viewDidLoad()
-        
             viewModel = MainViewModel(viewController: self)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        viewModel?.makeMark()
     }
     override func loadView() {
         let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
-        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
         view = mapView
         mapView.delegate = self
         // Creates a marker in the center of the map.
