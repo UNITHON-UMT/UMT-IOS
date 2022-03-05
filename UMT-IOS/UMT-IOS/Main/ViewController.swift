9f7f882e76267c801637314ec361f8bf2bdf1c5e
이진하 <dkwlsfk2323@gmail.com>
2022-03-05 16:36:04 +0900

    add project

---
+++ UMT-IOS/UMT-IOS/Main/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UMT-IOS
+//
+//  Created by 이진하 on 2022/03/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
